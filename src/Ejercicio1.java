cb67782dc9d0d81b6cbcc958af727b90f44d078a
JanireG <jgarciadi23dw@ikzubirimanteo.com>
2024-02-24 19:56:22 +0100

    Ejercicio1 terminado

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Ejercicio1 {
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+    /*
+    Lee una cadena y observando carácter a carácter sin poder retroceder, muestra por
+    pantalla cuantas veces está la sílaba ‘pa’.
+     */
+        Scanner sc = new Scanner(System.in);
+        int count=0;
+        System.out.println("Escribe una cadena:");
+        String s = sc.nextLine();
+        for (int i=0; i<s.length()-1; i++) {
+            if (s.charAt(i)=='p' && s.charAt(i+1)=='a') {
+                count++;
+            }
+        }
+
+        System.out.println("La sílaba 'pa' aparece " + count + " veces.");
+    }
 }
